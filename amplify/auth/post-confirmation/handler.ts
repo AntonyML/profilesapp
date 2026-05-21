19f373662e265082a0023ed54dd910527f7eb03e
ANTONY.MONGELOPEZ <antony.mongelopez@ucr.ac.cr>
2026-05-21 17:48:44 -0600

    Fix: Update import path for generateClient in post-confirmation handler

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,7 +1,7 @@
 import type { PostConfirmationTriggerHandler } from "aws-lambda";
 import { type Schema } from "../../data/resource";
 import { Amplify } from "aws-amplify";
-import { generateClient } from "aws-amplify/api";
+import { generateClient } from "aws-amplify/data";
 import { env } from "$amplify/env/post-confirmation";
 import { createUserProfile } from "./graphql/mutations";
 
